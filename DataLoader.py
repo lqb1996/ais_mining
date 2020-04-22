1d9e6d285d4d0f54b356e0ef74f8ccfe2c53af12
lqb1996 <lqb1139134758@163.com>
2020-04-22 18:42:45 +0800

    set_clomns

--- DataLoader.py
+++ DataLoader.py
@@ -9,74 +9,22 @@ import datetime
 class CSVDataLoader:
     def __init__(self, csv_path):
         self.csv_files = []
+        self.mmsi = []
+        self.sort_by_mmsi = {}
+        self.df_total = None
+        self.np_total = None
         for root, dirs, files in os.walk(csv_path):
             for f in files:
                 if f.endswith('.csv'):
                     self.csv_files.append(os.path.join(root, f))
-        self.read_feat(self.csv_files[0])
+        for csv_file in self.csv_files:
+            self.read_feat(csv_file)
+        for mmsi, group in self.df_total.groupby('mmsi'):
+            print(mmsi)
+            print(group)
 
     def read_feat(self, csv_file):
         df = pd.read_csv(csv_file)
         df = df.iloc[::-1]
-
-        # if test_mode:
-        #     df_feat = [df['渔船ID'].iloc[0], df['type'].iloc[0]]
-        #     df = df.drop(['type'], axis=1)
-        # else:
-        #     df_feat = [df['渔船ID'].iloc[0]]
-
-        df_feat = [df['mmsi'].iloc[0]]
-
-        df['time'] = df['time'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S"))
-        df_diff = df.diff(1).iloc[1:]
-        df_diff['time_seconds'] = df_diff['time'].dt.total_seconds()
-        df_diff['dis'] = np.sqrt(df_diff['longitude'] ** 2 + df_diff['latitude'] ** 2)
-
-        df_feat.append(df['time'].dt.day.nunique())
-        df_feat.append(df['time'].dt.hour.min())
-        df_feat.append(df['time'].dt.hour.max())
-        df_feat.append(df['time'].dt.hour.value_counts().index[0])
-
-        df_feat.append(df['sog'].min())
-        df_feat.append(df['sog'].max())
-        df_feat.append(df['sog'].mean())
-
-        # df_feat.append(df_diff['time'].min())
-        # df_feat.append(df_diff['time'].max())
-        # df_feat.append(df_diff['time'].mean())
-
-        df_feat.append(df_diff['sog'].min())
-        df_feat.append(df_diff['sog'].max())
-        df_feat.append(df_diff['sog'].mean())
-        df_feat.append((df_diff['sog'] > 0).mean())
-        df_feat.append((df_diff['sog'] == 0).mean())
-
-        df_feat.append(df_diff['cog'].min())
-        df_feat.append(df_diff['cog'].max())
-        df_feat.append(df_diff['cog'].mean())
-        df_feat.append((df_diff['cog'] > 0).mean())
-        df_feat.append((df_diff['cog'] == 0).mean())
-
-        df_feat.append((df_diff['longitude'].abs() / df_diff['time_seconds']).min())
-        df_feat.append((df_diff['longitude'].abs() / df_diff['time_seconds']).max())
-        df_feat.append((df_diff['longitude'].abs() / df_diff['time_seconds']).mean())
-        df_feat.append((df_diff['longitude'] > 0).mean())
-        df_feat.append((df_diff['longitude'] == 0).mean())
-
-        df_feat.append((df_diff['latitude'].abs() / df_diff['time_seconds']).min())
-        df_feat.append((df_diff['latitude'].abs() / df_diff['time_seconds']).max())
-        df_feat.append((df_diff['latitude'].abs() / df_diff['time_seconds']).mean())
-        df_feat.append((df_diff['latitude'] > 0).mean())
-        df_feat.append((df_diff['latitude'] == 0).mean())
-
-        df_feat.append(df_diff['dis'].min())
-        df_feat.append(df_diff['dis'].max())
-        df_feat.append(df_diff['dis'].mean())
-
-        df_feat.append((df_diff['dis'] / df_diff['time_seconds']).min())
-        df_feat.append((df_diff['dis'] / df_diff['time_seconds']).max())
-        df_feat.append((df_diff['dis'] / df_diff['time_seconds']).mean())
-        print(df_diff)
-        print(df_feat)
-
-        return df_feat
+        self.df_total = pd.concat([self.df_total, df])
+        return df
--- train.py
+++ train.py
@@ -12,6 +12,7 @@ from sklearn.model_selection import StratifiedKFold
 
 import lightgbm as lgb
 from DataLoader import *
+from plot import *
 
 proDir = os.path.split(os.path.realpath(__file__))[0]
 configPath = os.path.join(proDir, "setting.cfg")
@@ -19,3 +20,5 @@ cf = configparser.ConfigParser()
 cf.read(configPath)
 csv_path = cf.get("path", "csv_path")
 csv_loader = CSVDataLoader(csv_path)
+# plot(csv_loader.total['longitude'], csv_loader.total['latitude'])
+# print(csv_loader.df_total)
