e10bbd55d4ffd8dc8712c0561000599c27e544c8
lqb1996 <lqb1139134758@163.com>
2020-04-24 19:41:55 +0800

    set_clomns

--- DataLoader.py
+++ DataLoader.py
@@ -9,7 +9,7 @@ import datetime
 class CSVDataLoader:
     def __init__(self, csv_path):
         self.csv_files = []
-        self.mmsi = []
+        self.mmsi_list = []
         self.sort_by_mmsi = {}
         self.df_total = None
         self.np_total = None
@@ -19,9 +19,12 @@ class CSVDataLoader:
                     self.csv_files.append(os.path.join(root, f))
         for csv_file in self.csv_files:
             self.read_feat(csv_file)
-        for mmsi, group in self.df_total.groupby('mmsi'):
-            print(mmsi)
-            print(group)
+        mmsi_group = self.df_total.groupby('mmsi')
+        max_mmsi = mmsi_group.count()['time'].max()
+        for mmsi, group in mmsi_group:
+            self.mmsi_list.append(mmsi)
+            # print(group)
+        print(max_mmsi)
 
     def read_feat(self, csv_file):
         df = pd.read_csv(csv_file)
